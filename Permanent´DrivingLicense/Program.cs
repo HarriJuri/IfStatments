d667dd7759d2bedd22e93039cd9216929f56c7e6
Opilane TTHK <opilane@tthk.ee>
2021-09-07 11:34:39 +0300

    first commit

---
+++ Permanent´DrivingLicense/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Permanent_DrivingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm kontrollib kasutaja vanust
+            //programm teatab kaustajat, kas ta on piisavalt vana
+            //et juhiluba saada
+
+            Console.WriteLine("Mis aastal sa oled sündinud?")
+            int Age = Int32.Parse(Console.ReadLine());
+            int YearOfBirth = 2021 - Age;
+
+            if (Age > YearOfBirth)
+            {
+                Console.WriteLine("Oled piisavalt vana et saada juhiluba");
+            }
+            else if (Age < )
+            {
+                Console.WriteLine("Oled liiga noor");
+            }
+
+
+        }
+
+    }
+}
